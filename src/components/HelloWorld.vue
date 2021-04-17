be513220990afeca7d6844e8e7048345e080c585
david <ostermann.david@gmail.com>
2021-04-17 22:23:48 +0200

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="px-8 prose">
     <h1>{{ msg }}</h1>
-    <button class="px-3 py-2 border border-gray-300 rounded-md shadow" @click="increment">
+    <button role="button" name="count" class="px-3 py-2 border border-gray-300 rounded-md shadow" @click="increment">
       count is: {{ count }}
     </button>
     <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
-
     <p class="text-sm text-gray-700">vite-ts-tailwind-starter v{{ appVersion }}</p>
   </div>
 </template>
 
-<script setup lang="ts">
-import { computed, defineProps } from 'vue'
-
+<script lang="ts">
+import { computed, defineComponent, defineProps } from 'vue'
 import { useStore, Mutation } from '@/store/index'
 
-const store = useStore()
-const appVersion = store.state.version // not reactive!
-const count = computed(() => store.state.count)
-const props = defineProps<{
-  msg: string
-}>()
+export default defineComponent({
+  props: {
+    msg: String
+  },
+  setup(props) {
+    const store = useStore()
+    const appVersion = store.state.version // not reactive!
+    const count = computed(() => store.state.count)
 
-function increment() {
-  store.commit(Mutation.INCREMENT, 1)
-}
+    function increment() {
+      store.commit(Mutation.INCREMENT, 1)
+    }
+    return {
+      increment, count, appVersion
+    }
+  }
+})
+
+
+
 </script>
--- tests/e2e/integration/basic_spec.ts
+++ tests/e2e/integration/basic_spec.ts
@@ -3,7 +3,13 @@
 describe('Homepage', () => {
   it('Shows correct text', () => {
     cy.visit('/')
-
     cy.contains('h1', 'Hello Vue 3 + Vite + Tailwind CSS')
+  })
+
+  describe('when user click on button', () => {
+    it('should increment count', () => {
+      const button = cy.findByRole('button', { name: /count/ }).click()
+      button.should('contain.text', 'count is 0')
+    })
   })
 })
